26cf22a176d5fde257382686a23e20d86c4617a4
AltafFattah <altaf.fattah05@gmail.com>
2023-04-04 22:21:24 +0700

    fix bug

--- src/js/index.js
+++ src/js/index.js
@@ -65,7 +65,7 @@ const games = {
   },
   levelUp() {
     config.level += 1;
-    if (config.level % 5 === 0) {
+    if (config.level %5 === 0) {
       config.level5 += 1;
     }
     return false;
@@ -77,13 +77,22 @@ const games = {
     if (config.level === 3) {
       title.innerHTML = "ENJOY!!!";
     }
-    if(config.level > 3){
-      title.innerHTML = ''
+    if(config.level === 5){
+      title.innerHTML = 'AYO... 1 KALI LAGI NAIK LEVEL'
     }
     if (config.level5 === 2) {
       title2.style.opacity = "1";
       title2.style.visibility = "visible";
       title2.style.zIndex = "1";
+    }
+    if(config.level === 7){
+      title.innerHTML = 'YEY KAMU SUDAH NAIK LEVEL'
+    }
+    if(config.level === 9){
+      title.innerHTML = 'SETIAP 5X MAKAN KAMU AKAN NAIK LEVEL'
+    }
+    if(config.level === 11){
+      title.innerHTML = 'MAKASIH SUDAH BERMAIN'
     }
   },
   isWin() {
