79d2f95c86a3de5013c92a22df0d5ac4eeceda19
AltafFattah <altaf.fattah05@gmail.com>
2023-04-04 22:23:26 +0700

    fix error

--- src/js/index.js
+++ src/js/index.js
@@ -80,16 +80,16 @@ const games = {
     if(config.level === 5){
       title.innerHTML = 'AYO... 1 KALI LAGI NAIK LEVEL'
     }
-    if (config.level5 === 2) {
-      title2.style.opacity = "1";
-      title2.style.visibility = "visible";
-      title2.style.zIndex = "1";
-    }
     if(config.level === 7){
       title.innerHTML = 'YEY KAMU SUDAH NAIK LEVEL'
     }
     if(config.level === 9){
       title.innerHTML = 'SETIAP 5X MAKAN KAMU AKAN NAIK LEVEL'
+    }
+    if (config.level === 10) {
+      title2.style.opacity = "1";
+      title2.style.visibility = "visible";
+      title2.style.zIndex = "1";
     }
     if(config.level === 11){
       title.innerHTML = 'MAKASIH SUDAH BERMAIN'
